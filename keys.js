0353c09630ac6e81a13f8d8f1743e492ac7016c4
Ethan Zander <ethanzander13@gmail.com>
2017-08-26 23:41:08 -0600

    Added Keys

---
+++ keys.js
@@ -0,0 +1,11 @@
+exports.twitterKeys = {
+    consumer_key: "VsrlUkMlnfeU8cA1s5DYdydfD",
+    consumer_secret: "JYDCWCMl25kaEw1PGeErWLZeptFlRi29Sw25HcOWV5u2MWw6Cs",
+    access_token_key: "704260214-QsMA53Zyq3L34SMx3NZOhPtmBTEH4QU6KZG4Kesg",
+    access_token_secret: "BI9E6dOVe7QYkV3iZ1Loc8LLQ5AND5jyomkhq9RkE55H4",
+  }
+  exports.spotifyKeys = {
+      id:"8ad16fd2efaa4f74973a2cf8c4ffa53b",
+      secret:"69c4185448d94932bb928ca056aa235e"
+  }
+  exports.omdbKey = "40e9cece";
